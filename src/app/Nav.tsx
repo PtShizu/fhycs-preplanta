cf4dbda4207a989df372395645ef83ed7ed74016
Julian <ruben.julian.moreno.astorga@uabc.edu.mx>
2025-04-07 12:35:35 -0700

    arreglados Links que se convertían a a por bootstrap

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -6,17 +6,17 @@ function Nav(){
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <Link className="navbar-brand" href="/">FHyCS</Link>
+                <Link href="/" passHref legacyBehavior><span className="navbar-brand cursor-pointer">FHyCS</span></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                    <Link className="nav-link" aria-current="page" href="/">Home</Link>
+                    <Link href="/" passHref legacyBehavior><span className="navbar-brand cursor-pointer">Home</span></Link>
                     </li>
                     <li className="nav-item">
-                    <Link className="nav-link" href="/salones">Salones</Link>
+                    <Link href="/salones" passHref legacyBehavior><span className="navbar-brand cursor-pointer">Salones</span></Link>
                     </li>
                 </ul>
                 </div>
