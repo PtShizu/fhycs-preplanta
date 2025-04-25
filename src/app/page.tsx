007cd2d1b2fead3ba0c464812f64a126df00213b
Julian <ruben.julian.moreno.astorga@uabc.edu.mx>
2025-04-25 11:48:05 -0700

    Intento de login con Google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 
+'use client';
+
 import React from 'react';
 import Nav from './Nav';
 import Image from 'next/image';
 import styles from './page.module.css';
 import facultad from '../../facultad.jpg'
+import { supabase } from '@/lib/supabase-client';
 
 export default function Home() {
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+    });
+    if (error) console.error('Error al iniciar sesión:', error.message);
+  };
+
   return (
     <div>
       <Nav />
@@ -17,12 +27,14 @@ export default function Home() {
           priority
           className={styles.backgroundImage}
         />
-        <div id="g_id_onload"
-          data-client_id="YOUR_GOOGLE_CLIENT_ID"
-          data-login_uri="https://fhycs-preplanta.vercel.app/"
-          data-your_own_param_1_to_login="any_value"
-          data-your_own_param_2_to_login="any_value">
-      </div>
+        <div className={styles.overlay}>
+          <h1 className={styles.title}>Bienvenido a la FHYCS</h1>
+          <h5 className={styles.subtitle}>
+            Facultad de Humanidades y Ciencias Sociales
+          </h5>
+          <button className='btn btn-success' onClick={handleLogin}>
+            Iniciar sesión
+          </button>
         <div className={styles.overlay}>
           <h1 className={styles.title}>LICENCIATURAS</h1>
           <ul className={styles.list}>
@@ -38,5 +50,6 @@ export default function Home() {
         </div>
       </div>
     </div>
+  </div>
   );
 }
