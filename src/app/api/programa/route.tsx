bc671ba8882ef7d5c3f540ec8c3ee536ec0721a9
Julian <ruben.julian.moreno.astorga@uabc.edu.mx>
2025-04-29 12:16:37 -0700

    ...

--- src/app/api/programa/route.tsx
+++
@@ -1,9 +0,0 @@
-// app/api/programa/[id]/route.ts
-import { NextResponse } from 'next/server'
-import obtenerDatosPrograma from '@/app/actions/obtenerDatosPrograma'
-
-export async function GET(req: Request, { params }: { params: { id: string } }) {
-  const programaId = parseInt(params.id)
-  const datos = await obtenerDatosPrograma(programaId)
-  return NextResponse.json(datos)
-}
